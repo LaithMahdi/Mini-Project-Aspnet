5b4b0df2db8284906fa4da51682b83d5f943bf8b
LaithMahdi <mahdilaith380@gmail.com>
2026-03-26 10:50:29 +0100

    initialize project

---
+++ mini-project-aspnet/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mini_project_aspnet.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+    }
+}
---
+++ mini-project-aspnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mini_project_aspnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
